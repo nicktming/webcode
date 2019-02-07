4f84e7edd43cb74adffc661fc2eae21907bca1fc
nicktming <2279563278@qq.com>
2019-02-07 15:16:26 +0800

    update pv pvc

---
+++ springboot-docker/src/k8s/installation/heketi.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# http://rdcqii.hundsun.com/portal/article/827.html
+
+yum install heketi heketi-client -y
+
--- springboot-docker/src/k8s/pv-pvc/install_heketi.sh
+++ springboot-docker/src/k8s/pv-pvc/install_heketi.sh
@@ -4,3 +4,29 @@ yum install heketi heketi-client -y
 
 ssh-keygen -t rsa
 
+ssh-copy-id -i /root/.ssh/id_rsa.pub root@
+ssh-copy-id -i /root/.ssh/id_rsa.pub root@
+
+# vim /etc/heketi/heketi.json
+#"executor": "ssh",
+#
+#    "_sshexec_comment": "SSH username and private key file information",
+#    "sshexec": {
+#      "keyfile": "/root/.ssh/id_rsa",
+#      "user": "root",
+#      "port": "22",
+#      "fstab": "/etc/fstab"
+#    },
+
+# vim /usr/lib/systemd/system/heketi.service
+# -> User=root
+
+systemctl daemon-reload
+systemctl enable heketi
+systemctl start heketi
+
+journalctl -u heketi
+
+heketi-cli --server http://localhost:8089 topology load  --json=topology.json
+
+heketi-cli --server http://localhost:8089 volume create --size=10 --replica=2
